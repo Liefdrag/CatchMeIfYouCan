79da78772ecbb2b17f43479476a8d549ba67672a
Luke <lwjames1996@gmail.com>
2016-04-06 23:57:19 +0100

    Added Parser
    Added the client-side server packet parser. There are a few issues I
    commented with errors present in the code.

--- src/networking/packets/Packet.java
+++ src/networking/packets/Packet.java
@@ -3,192 +3,288 @@ package networking.packets;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
-public abstract class Packet {//will need to test for construction of packets, worked however when testing individually
-	
+import networking.packets.serverPackets.*;
+import networking.packets.serverPackets.broadcastPackets.*;
+import networking.packets.serverPackets.lobbyInfoPackets.*;
+
+public abstract class Packet {// will need to test for construction of packets,
+								// worked however when testing individually
+
 	protected byte[] packet = new byte[0];
-	
-	protected byte getByte(){
+
+	protected byte getByte() {
 		byte byte_ = packet[0];
 		packet = Arrays.copyOfRange(packet, 1, packet.length);
-		
-		//printPacket();
-		
+
+		// printPacket();
+
 		return byte_;
 	}
-	
-	protected void putByte(byte byte_){
+
+	protected void putByte(byte byte_) {
 		packet = Arrays.copyOf(packet, packet.length + 1);
 		packet[packet.length - 1] = byte_;
-		
-		//printPacket();
+
+		// printPacket();
 	}
-	
-	protected char getChar(){
+
+	protected char getChar() {
 		char character;
-		byte[] character_ = new byte[]{packet[0], packet[1]};
+		byte[] character_ = new byte[] { packet[0], packet[1] };
 		character = ByteBuffer.wrap(character_).getChar();
 		packet = Arrays.copyOfRange(packet, 2, packet.length);
-		
-		//printPacket();
-		
+
+		// printPacket();
+
 		return character;
 	}
-	
-	protected void putChar(char character){
+
+	protected void putChar(char character) {
 		byte[] character_ = new byte[2];
 		ByteBuffer.wrap(character_).putChar(character);
 		packet = Arrays.copyOf(packet, packet.length + 2);
 		packet[packet.length - 2] = character_[0];
 		packet[packet.length - 1] = character_[1];
-		
-		//printPacket();
+
+		// printPacket();
 	}
-	
-	protected double getDouble(){
+
+	protected double getDouble() {
 		double d_;
 		byte[] double_;
-		
+
 		double_ = Arrays.copyOf(packet, 8);
 		d_ = ByteBuffer.wrap(double_).getDouble();
 		packet = Arrays.copyOfRange(packet, 8, packet.length);
-		
-		//printPacket();
-		
+
+		// printPacket();
+
 		return d_;
 	}
-	
-	protected void putDouble(double d_){
+
+	protected void putDouble(double d_) {
 		byte[] double_ = new byte[8];
 		ByteBuffer.wrap(double_).putDouble(d_);
-		
+
 		packet = Arrays.copyOf(packet, packet.length + 8);
-		
-		for(int i = 0; i < 8; i ++){
-			packet[packet.length - (8-i)] = double_[i];
+
+		for (int i = 0; i < 8; i++) {
+			packet[packet.length - (8 - i)] = double_[i];
 		}
-		
-		//printPacket();
+
+		// printPacket();
 	}
-	
-	protected int getInt(){
+
+	protected int getInt() {
 		byte[] int_;
 		int integer;
 		int_ = Arrays.copyOf(packet, 4);
 		integer = ByteBuffer.wrap(int_).getInt();
 		packet = Arrays.copyOfRange(packet, 4, packet.length);
-		
-		//printPacket();
-		
+
+		// printPacket();
+
 		return integer;
 	}
-	
-	protected void putInt(int integer){
+
+	protected void putInt(int integer) {
 		byte[] int_ = new byte[4];
 		ByteBuffer.wrap(int_).putDouble(integer);
-		
+
 		packet = Arrays.copyOf(packet, packet.length + 4);
-		
-		for(int i = 0; i < 4; i ++){
-			packet[packet.length - (4-i)] = int_[i];
+
+		for (int i = 0; i < 4; i++) {
+			packet[packet.length - (4 - i)] = int_[i];
 		}
-		
-		//printPacket();
+
+		// printPacket();
 	}
-	
-	protected void putString(String string){//need to include padding in the event the string is not of a preset size.
+
+	protected void putString(String string) {// need to include padding in the
+												// event the string is not of a
+												// preset size.
 		char[] string_ = string.toCharArray();
-		int length =  string.length();
-		putInt(length);//putting the length of the string first
-		for(int i = 0; i < length; i++){
+		int length = string.length();
+		putInt(length);// putting the length of the string first
+		for (int i = 0; i < length; i++) {
 			putChar(string_[i]);
 		}
 	}
-	
-	protected String getString(){
+
+	protected String getString() {
 		String string_ = "";
-		int length = getInt();//getting the length of the string to know how much to read
-		for(int i = 0; i < length; i++){
+		int length = getInt();// getting the length of the string to know how
+								// much to read
+		for (int i = 0; i < length; i++) {
 			string_ += getChar();
 		}
-		
+
 		return string_;
 	}
-	
-	public byte[] getPacket(){//used to perform a checksum on a packet and return it.
-//		byte[] newPacket = new byte[packet.length + 1];
-//		byte checksum = 0x00;
-//		
-//		for(int i = 0; i < packet.length; i++){
-//			checksum += packet[i];
-//			newPacket[i] = packet[i];
-//		}
-//		newPacket[newPacket.length - 1] = checksum; //adding the checksum to the packet.
-//		
-//		System.out.println(toString() + checksum + " | ");
-//		
-//		return newPacket;
+
+	public byte[] getPacket() {// used to perform a checksum on a packet and
+								// return it.
+		// byte[] newPacket = new byte[packet.length + 1];
+		// byte checksum = 0x00;
+		//
+		// for(int i = 0; i < packet.length; i++){
+		// checksum += packet[i];
+		// newPacket[i] = packet[i];
+		// }
+		// newPacket[newPacket.length - 1] = checksum; //adding the checksum to
+		// the packet.
+		//
+		// System.out.println(toString() + checksum + " | ");
+		//
+		// return newPacket;
 		return packet;
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		String bytes = " | ";
-		for(int i = 0; i < packet.length; i ++){
+		for (int i = 0; i < packet.length; i++) {
 			bytes += packet[i] + " | ";
 		}
 		return bytes;
 	}
 
-	private void printPacket(){
+	private void printPacket() {
 		System.out.println(toString());
 	}
-	
+
+	// IDs are the same for some packets. These need to be changed.
+	public static Packet parse(byte[] bytes) {
+		switch (bytes[1]) {
+			case LOCATION:
+				return new LocationPacket(); // return packet
+			case PING:
+				return new PingPacket();
+			case BROADCAST:
+				return parseBroadcast(bytes);
+			case TARGET:
+				return new TargetPacket();
+			case SPAWN_REGION:
+				return new SpawnRegionPacket();
+			case ABILITY_USAGE:
+				return new AbilityUsagePacket();
+			case GAME_START:
+				return new GameStartPacket();
+			case GAME_END:
+				return new GameEndPacket();
+			case ROOM_CLOSE:
+				return new RoomClosePacket();
+			case ROOM_KEY:
+				return new RoomKeyPacket();
+			case LOBBYINFO:
+				return parseLobbyInfo(bytes);
+			case KICK:
+				return new KickPacket();
+			case NAK:
+				return new NAKPacket();
+			case HOST:
+				return new HostPacket();
+			default:
+				return null;
+		}
+	}
+
+	// Some LobbyInfo & Broadcast packets are the same, this is a problem
+	// Some packets with IDs don't exist.
+	private static Packet parseBroadcast(byte[] bytes) {
+		switch (bytes[1]) {
+			case BROADCAST_TIME_REMAINING:
+				return new TimeRemainingPacket();
+			case BROADCAST_LEADERBOARD:
+				return new LeaderboardPacket();
+			case BROADCAST_CAPTURE:
+				return new CapturePacket();
+			case BROADCAST_VOTES:
+				return new VotesPacket();
+			case BROADCAST_QUIT:
+				return new QuitPacket();
+			case BROADCAST_BOUNDARY_UPDATE:
+				return new BoundaryUpdatePacket();
+			case BROADCAST_NEW_HOST:
+				return new NewHostPacket();
+			case BROADCAST_NEW_PLAYER:
+				return new NewPlayerPacket();
+			case BROADCAST_PLAYER_READY:
+				return new PlayerReadyPacket();
+			default:
+				return null;
+		}
+	}
+
+	private static Packet parseLobbyInfo(byte[] bytes) {
+		switch (bytes[1]) {
+			case LOBBYINFO_GAMETYPE:
+				return new GametypePacket();
+			case LOBBYINFO_TIME_LIMIT:
+				return new TimeLimitPacket();
+			case LOBBYINFO_SCORE_LIMIT:
+				return new ScoreLimitPacket();
+			case LOBBYINFO_BOUNDARIES:
+				return new BoundariesPacket();
+			case LOBBYINFO_LEADERBOARD:
+				return new LeaderboardPacket();
+			case LOBBYINFO_ROOM_NAME:
+				return new RoomNamePacket();
+			case LOBBYINFO_VOTES:
+				return new VotesPacket();
+			default:
+				return null;
+		}
+	}
+
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 	/**
-	* PACKET DATA IDs
-	*/
-	
-	//Ability IDs
-	
-	//Hides the player location from their pursuer
+	 * PACKET DATA IDs
+	 */
+
+	// Ability IDs
+
+	// Hides the player location from their pursuer
 	public static final byte ABILITY_HIDE = 0X01;
-	//Produces beeps on target's device, i.e. if nearby you can hear the beep and find the target
+	// Produces beeps on target's device, i.e. if nearby you can hear the beep
+	// and find the target
 	public static final byte ABILITY_PING = 0X02;
-	//Sends a fake location to the pursuer for x amount of time or until pursuer enters within y meters of decoy
+	// Sends a fake location to the pursuer for x amount of time or until
+	// pursuer enters within y meters of decoy
 	public static final byte ABILITY_DECOY = 0X03;
-	//Target not alerted when pursuer is within x meters of target
+	// Target not alerted when pursuer is within x meters of target
 	public static final byte ABILITY_SNEAK = 0X04;
-	
-	//Gametype IDs
+
+	// Gametype IDs
 	public static final byte GAMETYPE_DEFAULT = 0X01;
 	public static final byte GAMETYPE_TEAM = 0X02;
 	public static final byte GAMETYPE_MAN_HUNT = 0X03;
-	
-	//NAK IDs
+
+	// NAK IDs
 	public static final byte NAK_INVALID_ROOM_KEY = 0x01;
 	public static final byte NAK_NOT_ENOUGH_PLAYERS = 0x02;
 	public static final byte NAK_ROOM_FULL = 0x03;
 	public static final byte NAK_NO_VALID_TARGETS = 0x04;
-	
-	//Disconnect IDs
+
+	// Disconnect IDs
 	public static final byte DISCONNECT_QUIT = 0X01;
 	public static final byte DISCONNECT_POOR_CONNECTION = 0X02;
 	public static final byte DISCONNECT_KICK = 0X03;
-	
-	//Votes IDs
-	public static final byte VOTES_ENABLED= 0x01;
-	public static final byte VOTES_DISABLED= 0x02;
-	
-	//Kick IDs
+
+	// Votes IDs
+	public static final byte VOTES_ENABLED = 0x01;
+	public static final byte VOTES_DISABLED = 0x02;
+
+	// Kick IDs
 	public static final byte KICK_POOR_CONNECTION = 0X01;
 	public static final byte KICK_KICKED = 0X02;
-	
-	//Location ID -- same for server and client
+
+	// Location ID -- same for server and client
 	public static final byte LOCATION = 0x01;
-	
+
 	/*----------------------------------------------------------*/
-	
-	//Client IDs
+
+	// Client IDs
 	public static final byte PING_RESPONSE = 0x02;
 	public static final byte CATCH_PERFORMED = 0x03;
 	public static final byte CAPTURED = 0x04;
@@ -201,8 +297,8 @@ public abstract class Packet {//will need to test for construction of packets, w
 	public static final byte ACK = 0x0B;
 	public static final byte BAD_SPAWN = 0x0C;
 	public static final byte PLAYER_READY = 0x0D;
-	
-	//Client Host Action IDs
+
+	// Client Host Action IDs
 	public static final byte HOST_ACTION_START_ROUND = 0x01;
 	public static final byte HOST_ACTION_END_ROUND = 0x02;
 	public static final byte HOST_ACTION_KICK_PLAYER = 0x03;
@@ -215,10 +311,10 @@ public abstract class Packet {//will need to test for construction of packets, w
 	public static final byte HOST_ACTION_BOUNDARY_UPDATES = 0x0A;
 	public static final byte HOST_ACTION_CHANGE_HOST = 0x0B;
 	public static final byte HOST_ACTION_ALLOW_VOTING = 0x0C;
-	
+
 	/*----------------------------------------------------------*/
-	
-	//Server IDs
+
+	// Server IDs
 	public static final byte PING = 0x02;
 	public static final byte BROADCAST = 0x03;
 	public static final byte TARGET = 0x04;
@@ -233,8 +329,8 @@ public abstract class Packet {//will need to test for construction of packets, w
 	public static final byte NAK = 0x0D;
 	public static final byte HOST = 0x0E;
 	public static final byte CAUGHT = 0x0F;
-	
-	//Broadcast IDs
+
+	// Broadcast IDs
 	public static final byte BROADCAST_TIME_REMAINING = 0x01;
 	public static final byte BROADCAST_LEADERBOARD = 0x02;
 	public static final byte BROADCAST_CAPTURE = 0x03;
@@ -244,8 +340,8 @@ public abstract class Packet {//will need to test for construction of packets, w
 	public static final byte BROADCAST_NEW_HOST = 0x07;
 	public static final byte BROADCAST_NEW_PLAYER = 0x08;
 	public static final byte BROADCAST_PLAYER_READY = 0x09;
-	
-	//Lobby Information IDs
+
+	// Lobby Information IDs
 	public static final byte LOBBYINFO_GAMETYPE = 0x01;
 	public static final byte LOBBYINFO_TIME_LIMIT = 0x02;
 	public static final byte LOBBYINFO_SCORE_LIMIT = 0x03;
