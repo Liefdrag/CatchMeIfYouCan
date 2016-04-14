dbe035b83407718b6ddcc321ee0afd8656739907
ljames96 <luke@138-38-190-58.eduroam.bath.ac.uk>
2016-04-14 10:45:44 +0100

    More game functionality
    Finished most of the Player/HostPlayer functions for the client to
    start using

--- src/game/HostPlayer.java
+++ src/game/HostPlayer.java
@@ -7,57 +7,75 @@ import networking.packets.clientPackets.hostPackets.*;
 public class HostPlayer extends Player{
 	
 	public HostPlayer() throws UnknownHostException{
-		super();
 	}
 	
-	//needs to be changed for names
-	public Game create(String roomName, String hostName, double[] address){
+	@Override
+	public void create(String roomName, String hostName, double[] address){
 		CreateRoomPacket packet = new CreateRoomPacket();
 		packet.putRoomName(roomName);
 		packet.putHostName(hostName);
 		packet.putMACAddress(address);
 		client.sendPacket(packet);
-		//wait for reply and return game object
-		return null;
 	}
 	
 	public void close(){
 		client.sendPacket(new CloseRoomPacket());
 	}
 	
-	public void useAbility(byte[] ability) {
-		// TODO Auto-generated method stub
-		
+	public void allowVoting(){
+		AllowVotingPacket packet = new AllowVotingPacket();
+		client.sendPacket(packet);
+	}
+	
+	public void updateBoundaries(int interval, int percentage){
+		BoundaryUpdatesPacket packet = new BoundaryUpdatesPacket();
+		packet.putBoundaryUpdates(interval, percentage);
+		client.sendPacket(packet);
+	}
+	
+	public void changeHost(int newHost){
+		ChangeHostPacket packet = new ChangeHostPacket();
+		packet.putNewHostID(newHost);
+	}
+	
+	public void endRound(){
+		EndRoundPacket packet = new EndRoundPacket();
+		client.sendPacket(packet);
 	}
 
-	public void captureTarget() {
-		// TODO Auto-generated method stub
-		
+	public void setGametype(byte gametype){
+		GametypePacket packet = new GametypePacket();
+		packet.putGameType(gametype);
+		client.sendPacket(packet);
 	}
-
-	public void playerCaptured() {
-		// TODO Auto-generated method stub
-		
+	
+	public void kick(int player){
+		KickPlayerPacket packet = new KickPlayerPacket();
+		packet.putPlayerID(player);
+		client.sendPacket(packet);
 	}
-
-	public void join(String roomKey) {
-		// TODO Auto-generated method stub
-		
+	
+	public void setScoreLimit(int scoreLimit){
+		ScoreLimitPacket packet = new ScoreLimitPacket();
+		packet.putScoreLimit(scoreLimit);
+		client.sendPacket(packet);
 	}
-
-	public void ready() {
-		// TODO Auto-generated method stub
-		
+	
+	public void setBoundaries(double longitude, double latitude, int radius){
+		SetBoundariesPacket packet = new SetBoundariesPacket();
+		packet.putBoundaries(longitude, latitude, radius);
+		client.sendPacket(packet);
 	}
-
-	public void quit() {
-		// TODO Auto-generated method stub
-		
+	
+	public void start(){
+		StartRoundPacket packet = new StartRoundPacket();
+		client.sendPacket(packet);
 	}
-
-	public void vote(byte[] data) {
-		// TODO Auto-generated method stub
-		
+	
+	public void setTimeLimit(int timeLimit){
+		TimeLimitPacket packet = new TimeLimitPacket();
+		packet.putTimeLimit(timeLimit);
+		client.sendPacket(packet);
 	}
-
+	
 }
--- src/game/Player.java
+++ src/game/Player.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import networking.client.Client;
+import networking.packets.clientPackets.*;
 
 /**
  * Client -> Server methods
@@ -18,25 +19,53 @@ public class Player {
 		game = null;
 	}	
 	
-	public void useAbility(byte[] ability) {
+	public void create(String roomKey, String playerName, double[] address){
+		JoinPacket packet = new JoinPacket();
+		packet.putRoomKey(roomKey);
+		packet.putPlayerName(playerName);
+		packet.putMACAddress(address);
+		client.sendPacket(packet);
+	}
+	
+	public void useAbility(byte ability) {
+		AbilityUsagePacket packet = new AbilityUsagePacket();
+		packet.putAbility(ability);
+		client.sendPacket(packet);
 	}
 	
 	public void captureTarget() {
+		CatchPerformedPacket packet = new CatchPerformedPacket();
+		client.sendPacket(packet);
 	}
 	
 	public void playerCaptured() {
-	}
-	
-	public void join(String roomKey) {
+		CapturedPacket packet = new CapturedPacket();
+		client.sendPacket(packet);
 	}
 	
 	public void ready() {
+		PlayerReadyPacket packet = new PlayerReadyPacket();
+		client.sendPacket(packet);
 	}
 	
 	public void quit() {
+		QuitPacket packet = new QuitPacket();
+		client.sendPacket(packet);
 	}
 	
-	public void vote(byte[] data) {
+	public void vote(byte vote) {
+		VotePacket packet = new VotePacket();
+		packet.putVote(vote);
+		client.sendPacket(packet);
 	}
 	
+	public void report(int player){
+		ReportPacket packet = new ReportPacket();
+		packet.report(player);
+		client.sendPacket(packet);
+	}
+	
+	public void setGame(Game game){
+		this.game = game;
+	}
 }
