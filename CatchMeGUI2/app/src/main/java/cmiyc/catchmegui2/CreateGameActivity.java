a38d5af07a830897a7d369bc3e34f0a59c787dc3
Liefdrag <adamhobbs123@gmail.com>
2016-04-28 00:09:17 +0100

    Added In-Game Options Functionality + Host Kick

--- CatchMeGUI2/app/src/main/java/cmiyc/catchmegui2/CreateGameActivity.java
+++ CatchMeGUI2/app/src/main/java/cmiyc/catchmegui2/CreateGameActivity.java
@@ -1,7 +1,9 @@
 package cmiyc.catchmegui2;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -40,6 +42,33 @@ public class CreateGameActivity extends AppCompatActivity implements UpdateRoomK
                 startActivity(i);
             }
         });
+        Button kickButton=(Button)findViewById(R.id.kickFriendButton);
+        kickButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                kickFriend();
+            }
+        });
+    }
+
+    private void kickFriend() {
+        final String[] players = {"Steve", "Bob", "Johnny", "Joe"}; //Need to get an array of players from Leaderboard
+        new AlertDialog.Builder(this)
+                .setSingleChoiceItems(players, 0, null)
+                //Currently kicks one at a time, could change it to multiple players at later date
+                //Need to be careful for host not to kick itself
+                .setPositiveButton("KICK A PLAYER", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        int position = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
+                        String kickedPlayer = players[position]; //Gives the kicked player name
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        //Do Nothing
+                    }
+                })
+                .show();
     }
 
 
--- CatchMeGUI2/app/src/main/java/cmiyc/catchmegui2/InGameOptionsActivity.java
+++ CatchMeGUI2/app/src/main/java/cmiyc/catchmegui2/InGameOptionsActivity.java
@@ -1,10 +1,17 @@
 package cmiyc.catchmegui2;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Liefdrag on 13/04/2016.
@@ -20,7 +27,12 @@ public class InGameOptionsActivity extends AppCompatActivity {
                 finish();
             }
         });
-
+        Button abilitiesButton =(Button)findViewById(R.id.abilitiesButton);
+        abilitiesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                chooseAbility();
+            }
+        });
         Button quitInGameButton =(Button)findViewById(R.id.quitInGameButton);
         quitInGameButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -48,11 +60,69 @@ public class InGameOptionsActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void chooseAbility() {
+        final String[] abilities = {"HIDE", "PING", "DECOY", "SNEAK"};
+        new AlertDialog.Builder(this)
+                .setSingleChoiceItems(abilities, 0, null)
+                .setPositiveButton("USE", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        int position = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
+                        String usedAbility = abilities[position]; //Gives the ability used
+                        //MAY WANT TO DISPLAY DIALOG FOR A BIT
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        //Do Nothing
+                    }
+                })
+                .show();
+    }
+
     public void reportPlayer() {
-        //Does some Report Player stuff
+        final String[] players = {"Steve", "Bob", "Johnny", "Joe"}; //Need to get an array of players from Leaderboard
+        new AlertDialog.Builder(this)
+                .setSingleChoiceItems(players, 0, null)
+                .setPositiveButton("REPORT", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        int position = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
+                        String reportPlayer = players[position]; //Gives the reported player name
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        //Do Nothing
+                    }
+                })
+                .show();
     }
 
     public void sentReport() {
-        //Sends Packet To Server
+        AlertDialog.Builder report = new AlertDialog.Builder(this);
+        report.setTitle("Report Bug");
+
+        final EditText input = new EditText (InGameOptionsActivity.this);
+        //Makes the EditText visible on Dialog and also allows for increase in size
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        //Puts the EditText into a dialog's view
+        report.setView(input);
+
+        report.setPositiveButton("SEND", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String report = input.getText().toString();
+                //Then send report to Server
+            }
+        });
+        report.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Do Nothing
+            }
+        });
+        report.show();
     }
 }
