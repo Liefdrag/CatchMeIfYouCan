28efe68ecbe944bea846e96a10d0e434f3da8334
Liefdrag <adamhobbs123@gmail.com>
2016-04-28 21:20:46 +0100

    Fixed How To Play layout

--- CatchMeGUI2/app/src/main/java/cmiyc/catchmegui2/HowToPlayActivity.java
+++ CatchMeGUI2/app/src/main/java/cmiyc/catchmegui2/HowToPlayActivity.java
@@ -1,6 +1,5 @@
 package cmiyc.catchmegui2;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,27 +15,49 @@ public class HowToPlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.how_to_play);
         TextView howToPlay = (TextView)findViewById(R.id.howToPlayText);
-        String play = "Main Menu: Press Create Game to create a new lobby where you become the host. \n" +
-                "Main Menu: Press Join Game to go to a page where you can enter a room key to join a game \n" +
-                "Main Menu: Press Player Details to change your name and sign in your Google Account \n" +
-                "(HOST) Create Game: Press Invite Player to go to a screen where you can send the room key to specified Players \n" +
-                "(HOST) Create Game: Press Kick Player to display a dialog box to choose a Player to kick \n" +
-                "(HOST) Create Game: Press Customise Settings to change the game settings \n" +
-                "(HOST) Create Game: Press See Lobby to see the lobby \n" +
-                "(HOST) Lobby: Swipe right to see all the Players in the lobby in the leaderboard, then swipe back left to go back to lobby \n" +
-                "(HOST) Lobby: Press Back to go back to the previous screen, press Leave Game to leave the game and press Start Game to start the game \n" +
-                "In-Game: Swipe right to see the leaderboard then swipe back left to return to main game screen. Swipe left to see the map of your position and a circular boundary" +
-                "and then swipe back right to return to main screen. \n" +
-                "In-Game: Press Caught Target when you see your target to catch them \n" +
-                "In-Game: Been Caught button will only be enabled when your pursuer presses their caught target button. Once you have been caught then you should press the Been Caught button \n" +
-                "In-Game: Press options to navigate to an options page where you can use abilities, report a player, report a bug and leave the game \n" +
-                "In-Game: The compass on the main screen will direct you to your target. Each time you catch your target, your score will increase and your target will change. \n" +
-                "Join Game: Enter a room key to join the lobby of a game. If successful then the player can vote for the game mode of their choosing. Afterwards they will enter the lobby and will need to wait for when the host starts the game \n" +
-                "Player Details: The player can change their displayed name or can sign in with their Google Account and use that name. \n\n" +
-                "Aim of Game: \n" +
+
+        String play = "Aim of Game: \n" +
                 "Capture the most players and try to escape capture. \n" +
+                "----------\n"+
                 "Tip: Try to be sneaky and quiet. The quieter you are, the hard it is to spot you \n\n" +
-                "HEALTH AND SAFETY: MAKE SURE YOU ARE AWAKE OF YOUR SURROUNDING ENVIRONMENT AND THE PUBLIC. DO NOT PLAY IF YOU WILL DISTURB PEOPLE WHO AREN'T PLAYING THE GAME";
+                "----------\n"+
+                "HEALTH AND SAFETY: MAKE SURE YOU ARE AWAKE OF YOUR SURROUNDING ENVIRONMENT AND THE PUBLIC.\n DO NOT PLAY IF YOU WILL DISTURB PEOPLE WHO AREN'T PLAYING THE GAME\n" +
+                "----------\n"+
+                "----------\n"+
+                "Navigation:\n"+
+                "Main Menu: Press Create Game to create a new lobby where you become the host. \n" +
+                "----------\n"+
+                "Main Menu: Press Join Game to go to a page where you can enter a room key to join a game \n" +
+                "----------\n"+
+                "Main Menu: Press Player Details to change your name and sign in your Google Account \n" +
+                "----------\n"+
+                "(HOST) Create Game: Press Invite Player to go to a screen where you can send the room key to specified Players \n" +
+                "----------\n"+
+                "(HOST) Create Game: Press Kick Player to display a dialog box to choose a Player to kick \n" +
+                "----------\n"+
+                "(HOST) Create Game: Press Customise Settings to change the game settings \n" +
+                "----------\n"+
+                "(HOST) Create Game: Press See Lobby to see the lobby \n" +
+                "----------\n"+
+                "(HOST) Lobby: Swipe right to see all the Players in the lobby in the leaderboard, then swipe back left to go back to lobby \n" +
+                "----------\n"+
+                "(HOST) Lobby: Press Back to go back to the previous screen, press Leave Game to leave the game and press Start Game to start the game \n" +
+                "----------\n"+
+                "In-Game: Swipe right to see the leaderboard then swipe back left to return to main game screen. Swipe left to see the map of your position and a circular boundary" +
+                "----------\n"+
+                "and then swipe back right to return to main screen. \n" +
+                "----------\n"+
+                "In-Game: Press Caught Target when you see your target to catch them \n" +
+                "----------\n"+
+                "In-Game: Been Caught button will only be enabled when your pursuer presses their caught target button. Once you have been caught then you should press the Been Caught button \n" +
+                "----------\n"+
+                "In-Game: Press options to navigate to an options page where you can use abilities, report a player, report a bug and leave the game \n" +
+                "----------\n"+
+                "In-Game: The compass on the main screen will direct you to your target. Each time you catch your target, your score will increase and your target will change. \n" +
+                "----------\n"+
+                "Join Game: Enter a room key to join the lobby of a game. If successful then the player can vote for the game mode of their choosing. Afterwards they will enter the lobby and will need to wait for when the host starts the game \n" +
+                "----------\n"+
+                "Player Details: The player can change their displayed name or can sign in with their Google Account and use that name. \n\n";
         howToPlay.setText(play);
 
         Button backHowToPlayButton =(Button)findViewById(R.id.backHowToPlayButton);
